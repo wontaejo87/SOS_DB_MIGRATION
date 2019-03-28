9bc7e2af014356e53708232c716fafe99fe99eff
kindler0703 <yongjik.kim@yanolja.com>
2019-03-29 02:39:56 +0900

    init project

---
+++ src/main/java/com/yanolja/sos/migration/MigrationApplication.java
@@ -0,0 +1,18 @@
+package com.yanolja.sos.migration;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MigrationApplication implements CommandLineRunner {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MigrationApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("args = [" + args + "]");
+    }
+}
